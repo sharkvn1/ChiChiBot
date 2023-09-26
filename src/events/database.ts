949e04698bb3d24d89d1d88849de0239f3c382ff
sharkvn1 <richberchannel01@gmail.com>
2023-09-26 23:40:11 +0700

    - add database load for every server

---
+++ src/events/database.ts
@@ -0,0 +1,21 @@
+import { Events, Client } from 'discord.js';
+import guildConfig from '../database/guildConfig';
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    async execute(chichi: Client) {
+        const data = chichi.guilds.cache.toJSON();
+        for (const i in data) {
+            await guildConfig.findOne({
+                guildId: data[i].id
+            }).then((Data: any) => {
+                if (Data === null) {
+                    new guildConfig({
+                        guildId: data[i].id,
+                        WelcomeChannelId: 0
+                    }).save();
+                }
+            });
+        }
+    }
+}
